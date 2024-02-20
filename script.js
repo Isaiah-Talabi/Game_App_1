f57a834bc7e74591aa7a5422cdb7e7b8501887ce
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2024-02-20 19:15:04 +0100

    start

---
+++ script.js
@@ -0,0 +1,9 @@
+let score = 0;
+
+document.getElementById('click-btn').addEventListener('click', () => {
+  score++;
+  document.getElementById('score-value').textContent = score;
+});
+
+// You can add more games and functionalities here
+
